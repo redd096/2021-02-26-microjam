b509790b669fe566abd6880ecaa645d049ad8f90
redd096 <martin.babino96@gmail.com>
2021-02-26 10:33:52 +0100

    iniziato progetto 2d

---
+++ Assets/Scripts/PlayerRotator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotator : MonoBehaviour
+{
+    [Header("Commands")]
+    [SerializeField] KeyCode rightInput = KeyCode.RightArrow;
+    [SerializeField] KeyCode leftInput = KeyCode.LeftArrow;
+
+    [Header("Rotation")]
+    [SerializeField] float speedRotation = 30;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //press right to rotate on right
+        if(Input.GetKeyDown(rightInput))
+        {
+            rb.AddTorque(Vector3.right * speedRotation, ForceMode.Impulse);
+        }
+        //or left to rotate on left
+        else if(Input.GetKeyDown(leftInput))
+        {
+            rb.AddTorque(Vector3.left * speedRotation, ForceMode.Impulse);
+        }
+    }
+}
